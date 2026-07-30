1963148212e2c3e147c49d65879f5e99376c8064
broder33 <richardbroder@gmail.com>
2026-07-30 13:53:16 -0300

    0.5.99

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agenda-v0596';
+const CACHE_NAME = 'agenda-v0599';
 
 self.addEventListener('message', function(event) {
   if (event.data && event.data.type === 'SKIP_WAITING') {
